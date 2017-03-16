9a9eaeb580667ea029ef8c6a4d69b9d5510da2d5
vtboyarc <adamrcarter@gmail.com>
2017-03-16 16:59:12 -0500

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SimpleZen
+//
+//  Created by Adam Carter on 3/16/17.
+//  Copyright © 2017 Adam Carter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var quoteLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
