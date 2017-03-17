9159a5ee53efef966c15290cc01b968e5d1a219e
vtboyarc <adamrcarter@gmail.com>
2017-03-17 13:22:38 -0500

    added color model and logic for random colors

--- ViewController.swift
+++ ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
     //right click on label in IB and drag Reference Outlet
     @IBOutlet weak var quoteLabel: UILabel!
     
+    @IBOutlet weak var newQuoteButton: UIButton!
+    
+    
     //creating a constant to access the quotes in the QuoteProvider struct
     let quoteProvider = QuoteProvider()
+    
+    //ColorModel is the name of the struct
+    let colorModel = ColorModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +36,9 @@ class ViewController: UIViewController {
     //control + click and drag from button in IB and drop here, choose action
     @IBAction func newQuote() {
         quoteLabel.text = quoteProvider.randomQuote()
+        let randomColor = colorModel.randomColor()
+        view.backgroundColor = randomColor
+        newQuoteButton.tintColor = randomColor
     }
 
 }
