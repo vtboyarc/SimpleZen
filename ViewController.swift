ba71e83e3d6ee0098ef4a4f152318e9c7c677cc9
vtboyarc <adamrcarter@gmail.com>
2017-03-17 11:47:16 -0500

    created a new file with a struct for the quotes, added them in from the old version

--- ViewController.swift
+++ ViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 class ViewController: UIViewController {
     //right click on label in IB and drag Reference Outlet
     @IBOutlet weak var quoteLabel: UILabel!
-    let quotes = ["quote1", "quote 2", "quote 3"]
+    //creating a constant to access the quotes in the QuoteProvider struct
+    let quoteProvider = QuoteProvider()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        quoteLabel.text = quotes[0]
+        //quotes is the name of the array in the QuoteProvider struct, getting the first item
+        quoteLabel.text = quoteProvider.quotes[0]
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,7 +29,7 @@ class ViewController: UIViewController {
     //control + click and drag from button in IB and drop here, choose action
     @IBAction func newQuote() {
         //stubbing some mock data for now
-        quoteLabel.text = quotes[1]
+        quoteLabel.text = quoteProvider.quotes[1]
     }
 
 }
